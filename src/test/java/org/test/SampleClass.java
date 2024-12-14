d7e14f053d8dde51f8f94bb64723143d6dfcddd9
sathyaraj17 <sathya.sathyaraj2020@gmail.com>
2024-12-14 15:49:21 +0530

    Project Stucture

---
+++ src/test/java/org/test/SampleClass.java
@@ -0,0 +1,13 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class SampleClass {
+	
+	@Test
+	public void test1() {
+		
+
+	}
+
+}
---
+++ src/test/java/snippet/Snippet.java
@@ -0,0 +1,14 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		<!-- https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager -->
+		<dependency>
+		    <groupId>io.github.bonigarcia</groupId>
+		    <artifactId>webdrivermanager</artifactId>
+		    <version>5.6.3</version>
+		</dependency>
+		
+	}
+}
+
